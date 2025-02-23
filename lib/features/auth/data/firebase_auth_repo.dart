fac71f25c73421e07c4e3f595b1c2452b992307f
Ravid Krisi <ravidkrisi22@gmail.com>
2025-02-23 14:25:06 +0700

    implemented firebase_auth_repo

--- lib/features/auth/data/firebase_auth_repo.dart
+++ lib/features/auth/data/firebase_auth_repo.dart
@@ -1,23 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:social_app/features/auth/domain/entities/app_user.dart';
 import 'package:social_app/features/auth/domain/repos/auth_repo.dart';
 
 class FirebaseAuthRepo implements AutoRepo {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
   @override
-  Future<AppUser?> getCurrentUser() {
-    // TODO: implement getCurrentUser
-    throw UnimplementedError();
+  Future<AppUser?> getCurrentUser() async {
+    // get current logged in user in firebase
+    final firebaseUser = firebaseAuth.currentUser;
+
+    // no user logged in
+    if (firebaseUser == null) {
+      return null;
+    }
+
+    // user exists
+    return AppUser(uid: firebaseUser.uid, email: firebaseUser.email!, name: '');
   }
 
   @override
-  Future<AppUser?> loginWithEmailPassword(String email, String password) {
-    // TODO: implement loginWithEmailPassword
-    throw UnimplementedError();
+  Future<AppUser?> loginWithEmailPassword(String email, String password) async {
+    try {
+      // attempt sign in
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      // create user
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: '',
+      );
+
+      // return user
+      return user;
+    } catch (e) {
+      throw Exception('Login Failed: $e');
+    }
   }
 
   @override
-  Future<void> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<void> logout() async {
+    await firebaseAuth.signOut();
   }
 
   @override
@@ -25,8 +50,23 @@ class FirebaseAuthRepo implements AutoRepo {
     String name,
     String email,
     String password,
-  ) {
-    // TODO: implement registerWithEmailPassword
-    throw UnimplementedError();
+  ) async {
+    try {
+      // attempt sign up
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      // create user
+      AppUser user = AppUser(
+        uid: userCredential.user!.uid,
+        email: email,
+        name: name,
+      );
+
+      // return user
+      return user;
+    } catch (e) {
+      throw Exception('Register Failed: $e');
+    }
   }
 }
